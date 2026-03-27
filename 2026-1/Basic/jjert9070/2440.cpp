e90462db37970adc06b8f8cb103789d25dc78fdc
jjert9070 <202446129@jbnu.ac.kr>
2026-03-28 01:10:42 +0900

    update

---
+++ 2026-1/Basic/jjert9070/2440.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    int line;
+    int i, j;
+    cin>>line;
+
+    for(i = 0; i < line; i++)
+    {
+        for(j = 0; j < line - i; j++)
+        {
+            cout << '*';
+        }    
+        cout<<'\n';
+    }
+
+    return 0;
+}
